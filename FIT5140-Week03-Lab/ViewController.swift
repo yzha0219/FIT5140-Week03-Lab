64bcd6a47dd606bb94f46c603fcb8dff7c67eb76
Yi Zhang <cattyzhang0110@gmail.com>
2019-08-15 14:23:33 +1000

    Initial Commit

---
+++ FIT5140-Week03-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FIT5140-Week03-Lab
+//
+//  Created by Yi Zhang on 15/8/19.
+//  Copyright © 2019 Yi Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
